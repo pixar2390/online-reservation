84a6d89e494fe505a4dafc6fca845c5d0223a4f7
pixar2390 <pixar_duffy@icloud.com>
2020-06-09 14:19:03 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "reservations#index"
+  resources :users, only: [:index, :edit, :update,]
+  resources :reservations, only: [:index, :new, :create, :edit, :update,]
 
 
 end
