2930aca243c48006c3e1b2cf107987cb4229a185
pixar2390 <pixar_duffy@icloud.com>
2020-06-09 15:41:29 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #スペースなどの空白文字列で名前登録をしないようにバリデーションをかける
+  validates :name, presence: true
 end
