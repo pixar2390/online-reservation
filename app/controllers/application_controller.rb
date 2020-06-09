b3daa361e5bf61ad8a453d15fbfad3682cd47847
pixar2390 <pixar_duffy@icloud.com>
2020-06-09 15:41:16 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  #CSRF対策
+  protect_from_forgery with: :exception
+  #ユーザーの名前を登録できるようする
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
