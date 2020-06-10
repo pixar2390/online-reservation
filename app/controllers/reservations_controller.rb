00965365e58c7f22f63d201b5ea4484aafc87a18
pixar2390 <pixar_duffy@icloud.com>
2020-06-10 18:24:10 +0900

    Update reservations_controller.rb

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,11 @@
 class ReservationsController < ApplicationController
 
   def index
-    #userのID番号が１の時管理者専用ページに行くように設定する
-    if current_user.id < 2 then
+    #userのID番号が1~5までは管理者専用ページに行くように設定する
+    if current_user.id < 6 then
       redirect_to  edit_reservation_path(current_user.id)
     end
-    @current_num = Reservation.first.examination
+    @current_num = Reservation.first
     @next_num = Reservation.count + 1
   end
 
@@ -17,16 +17,31 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    exam_num = Reservation.count + 1
+    #もし受付件数が０ならばダミーデータを作成する
+    binding.pry
+    if Reservation.count == 0 then
+      (1..5).each{ |n|
+        dummy = Reservation.new(examination: n)
+        dummy.save
+      }
+    end
+    #ユーザーの情報をレコードに記録
+    if Reservation.count == 0 then
+      exam_num = 1
+    else
+      exam_num = Reservation.last.examination +1
+    end
     reservation = Reservation.new do |r|
       r.examination = exam_num
       r.user_id = current_user.id
     end
-    if reservation.save
+
+    if reservation.save == true then
       redirect_to root_path, notice: '受付が完了しました'
     else
       render :new
     end
+    
   end
 
   def edit
