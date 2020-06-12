f98e6b017b0f1e7e235b5717f554909ea42701fe
pixar2390 <pixar_duffy@icloud.com>
2020-06-12 14:00:57 +0900

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def notice_email
-    @user = params[:user]
+  def notice_email(user)
+    @user = user
     mail(to: @user.email, subject: '病院からのお知らせ')
   end
 end
