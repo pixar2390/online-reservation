ca5b776b40a8d59f8c48a633e17d2f1149440339
pixar2390 <pixar_duffy@icloud.com>
2020-06-12 14:00:52 +0900

    Update reservations_controller.rb

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,11 +5,11 @@ class ReservationsController < ApplicationController
     if current_user.id < 6 then
       redirect_to  edit_reservation_path(current_user.id)
     end
-    #テーブルの中身がからだと正しい診察番号が表示されないため条件分岐
+    #テーブルの中身が空だと正しい診察番号が表示されないため条件分岐
     if Reservation.count == 0 then
-      @current_num = "現在、診察中の方はいません。"
-      @next_num = 1
-    else
+      @current_num = "現在、休診中です。"
+      @next_num = "現在、ネットでの受付は受理出来かねます。"
+    else  #受付表に患者がいる場合
       @current_num = Reservation.first.examination
       @next_num = Reservation.count + 1
     end
@@ -23,26 +23,33 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    #もし受付件数が０ならばダミーデータを作成する
-    if Reservation.count == 0 then
-      (1..5).each{ |n|
-        dummy = Reservation.new(examination: n, user_id: n)
-        dummy.save
-      }
+    if current_user.id <= 5 then #病院関係者の場合
+      #もし受付件数が０ならばダミーデータを作成する
+      if Reservation.count == 0 then
+        (1..5).each{ |n|
+          dummy = Reservation.new(examination: n, user_id: n, flag: false)
+          dummy.save
+        }
+      end
+      #受付表編集画面に遷移
+      render :edit
+    else  #患者の場合
+      #受付済みのユーザーが再度受付すると一意性制約のせいでエラーが起こるので、ここでエスケープ,且レコードが0の時は休診中と判断し受付をさせない
+      if Reservation.where(user_id: current_user.id).empty? && Reservation.count != 0 then
+        #ユーザーの情報をレコードに記録
+        exam_num = Reservation.last.examination + 1
+        reservation = Reservation.new do |r|
+          r.examination = exam_num
+          r.user_id = current_user.id
+          r.flag = false
+        end
+        reservation.save
+      else
+        #アクション名指定すると、そのアクション名のテンプレートが呼ばれるだけで、コントローラーは読まれないのでインスタンス変数などを使っている場合は注意が必要。
+        render :new
+      end
     end
-    #ユーザーの情報をレコードに記録
-    exam_num = Reservation.last.examination + 1
-    reservation = Reservation.new do |r|
-      r.examination = exam_num
-      r.user_id = current_user.id
-    end
-    #受付済みのユーザーが再度受付すると一意性制約のせいでエラーが起こるので、ここでエスケープ
-    if Reservation.where(user_id: current_user.id).empty? then
-      reservation.save
-      render "finish"
-    else
-      render :new
-    end
+
     
   end
 
@@ -52,10 +59,13 @@ class ReservationsController < ApplicationController
   end
 
   def update
-    
+
   end
 
   def destroy
+    #あとで書く
+    UserMailer.notice_email(current_user).deliver_now
+    #メールを送ったら、flagの値を編集
     
   end
 
